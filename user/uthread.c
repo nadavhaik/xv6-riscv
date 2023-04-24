d2802dc26ac4c1a0d510b5ac130528e7589e9f32
nadavh <nadavhb@gmail.com>
2023-04-24 18:14:35 +0000

    uthread changes

--- user/uthread.c
+++ user/uthread.c
@@ -1,4 +1,5 @@
 #include "user/uthread.h"
+#include "user/user.h"
 
 static int uthreads_initialized = 0;
 static struct uthread uthreads[MAX_UTHREADS];
@@ -12,7 +13,6 @@ void initialize_uthreads()
     uthreads_initialized = 1;
 }
 
-
 int uthread_create(void (*start_func)(), enum sched_priority priority)
 {
     if(!uthreads_initialized) initialize_uthreads();
@@ -23,8 +23,14 @@ int uthread_create(void (*start_func)(), enum sched_priority priority)
 
         // else - t is a suitable thread entry in the table
         t->context.ra = (uint64) start_func;
-        t->context.sp = (uint64) &t->ustack;
         t->priority = priority;
+
+        memcpy(t->ustack, &uthread_exit, sizeof(&uthread_exit));
+        memcpy(&t->ustack[sizeof(&uthread_exit)], start_func, sizeof(start_func));
+        t->context.sp = (uint64)&t->ustack[sizeof(&uthread_exit)];
+        // t->context.sp = (uint64)&t->ustack[sizeof(&uthread_exit) + sizeof(start_func)];
+        // t->context.sp = (uint64)&t->ustack[sizeof(&uthread_exit)];
+        
         t->state = RUNNABLE;
         return 0;
     }
