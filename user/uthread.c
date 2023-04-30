dbaff222bc94a16390727ac8a583c89fe98a5ad4
nadavh <nadahb@gmail,com>
2023-04-30 09:58:49 +0000

    NULL check

--- user/uthread.c
+++ user/uthread.c
@@ -122,7 +122,7 @@ void uthread_yield()
     {
         struct uthread* t = *tpp;
         struct uthread* current = uthread_self();
-        current->state = RUNNABLE;
+        if(current != NULL) current->state = RUNNABLE;
         t->state = RUNNING;
         set_current_thread(t);
         uswtch(&current->context, &t->context);
