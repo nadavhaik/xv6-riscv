5ef26eb8bde594c2641376f7cbbbd374174ec657
nivkap <84071374+nivkap@users.noreply.github.com>
2023-05-30 12:18:38 +0000

    fixing the place

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -16,7 +16,6 @@ r_mhartid()
 #define MSTATUS_MPP_S (1L << 11)
 #define MSTATUS_MPP_U (0L << 11)
 #define MSTATUS_MIE (1L << 3)    // machine-mode interrupt enable.
-#define PTE_PG (1 L << 9) // Swapped out
 
 static inline uint64
 r_mstatus()
@@ -344,6 +343,7 @@ typedef uint64 *pagetable_t; // 512 PTEs
 #define PTE_W (1L << 2)
 #define PTE_X (1L << 3)
 #define PTE_U (1L << 4) // user can access
+#define PTE_PG (1L << 9) // Swapped out
 
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) ((((uint64)pa) >> 12) << 10)
